64e11ffcc7a6d1c68b974af7ea89b69444998902
oulcan <oulcan@oulcan-pc>
2013-03-23 22:19:22 +0200

    first commit

---
+++ AugmentedReality/jni/Android.mk
@@ -0,0 +1,8 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := AugmentedReality
+LOCAL_SRC_FILES := AugmentedReality.cpp
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ AugmentedReality/obj/local/armeabi/objs/AugmentedReality/AugmentedReality.o.d
@@ -0,0 +1,5 @@
+obj/local/armeabi/objs/AugmentedReality/AugmentedReality.o: \
+ jni/AugmentedReality.cpp \
+ D:/Development/android-ndk/platforms/android-14/arch-arm/usr/include/jni.h
+
+D:/Development/android-ndk/platforms/android-14/arch-arm/usr/include/jni.h:
